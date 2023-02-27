f0939af8af69667e704e4440480523631fc920ca
fritziyeke12 <fritziyeke@gmail.com>
2023-02-27 11:06:48 +0000

    added middleware and CUD endpoints

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
+
+app.use(express.json());
+
 app.get("/restaurants", async (req, res) => {
     const ret = await Restaurant.findAll();
     res.json(ret);
@@ -14,6 +17,28 @@ app.get("/restaurants", async (req, res) => {
 app.get('/restaurants/:id', async (req, res) => {
     const rest = await Restaurant.findByPk(req.params.id);
     res.json(rest);
+})
+
+app.post('/restaurants/add', async (req, res) => {
+    const rest = await Restaurant.create(req.body);
+    const all = await Restaurant.findAll();
+    res.send(all);
+})
+
+app.put('/restaurants/:id', async (req, res) => {
+    const rest = await Restaurant.findByPk(req.params.id);
+    await rest.update(req.body);
+    res.send(rest);
+})
+
+app.delete('/restaurants/:id', async (req, res) => {
+    await Restaurant.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    const all = await Restaurant.findAll();
+    res.send(all);
 })
 
 app.listen(port, () => {
